106bc80a9c494c4b3ac72bd492b9952b6738acb0
ethanb01 <barkate.ethan@gmail.com>
2018-10-11 21:17:26 +0300

    test the functions

--- List.c/main.c
+++ List.c/main.c
@@ -4,11 +4,12 @@
 
 int main() {
 	List* lst = new_list();
-	add_value(lst, 10);
-	add_value(lst, 11);
-	add_first(lst, 111);
-	reverse_list(lst);
+	add_value(lst, 2);
+	add_value(lst, 4);
+	add_first(lst, 5);
+	//reverse_list(lst);
 	print_list(lst);
+	//printf("\n%d \n", number_events(lst));
 	
 	return EXIT_SUCCESS;
 }
